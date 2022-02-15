b9fa0354eb7c65e64617c9f62e14de3c51995d6b
QSmally <github@qbot.eu>
2022-02-15 19:44:30 +0100

    Programs: add unoptimised shift/add multiplication

---
+++ Multiplication/multiplication.s
@@ -0,0 +1,27 @@
+@PAGE 0 0
+
+@DECLARE input_a 1
+@DECLARE input_b 2
+@DECLARE incrementer 3
+@DECLARE output 4
+
+; main
+    IMM @input_a, 5
+    IMM @input_b, 3
+    IMM @incrementer, -1
+    IMM @output, 0
+.loop:
+    INC @incrementer
+    AST @input_a
+    BRH #zero, .finished
+    RSH @input_a
+    BRH #!underflow, .loop
+; shift and add
+    AST @input_b
+    BPL @incrementer
+    ADD @output
+    RST @output
+    JMP 0, .loop
+.finished:
+    ; see @output for the result
+    JMP 0, .finished
