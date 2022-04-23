fcfb5b02d72fba7f65b26ae00dff2eac5eeaccd1
QSmally <github@qbot.eu>
2022-04-23 22:33:07 +0200

    Programs: add string occurrences

---
+++ String Occurrences/array.s
@@ -0,0 +1,5 @@
+@PAGE 0 1
+
+.&strocc.array:
+    $Hello world
+    0x00
---
+++ String Occurrences/strocc.s
@@ -0,0 +1,23 @@
+@PAGE 0 0
+
+@DECLARE location 1
+@DECLARE character 2
+
+@IF enable-prefetch
+    PRF .strocc.array-
+@END
+
+; main
+    IMM @location, .strocc.array
+    IMM @character, $l
+.loop:
+    MLI @location, 0
+    BRH #zero, .exit
+    SUB @character
+    BRH #!zero, .loop
+; add char
+    AST @location
+    DEC accumulator
+    PPS accumulator
+    JMP zero, .loop
+.exit:
